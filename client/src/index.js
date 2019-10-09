03109f660610c893510831966a538855214791b7
Anish <iamanishagarwal@gmail.com>
2019-10-09 19:26:35 +0530

    converted the screens in ejs to react

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import HomeScreen from "./screens/HomeScreen";
 import GameScreen from "./screens/GameScreen";
+import "./css/style_global.css";
 
 ReactDOM.render(
   <>
     <BrowserRouter>
       <Route path="/" exact component={HomeScreen} />
-      <Route path="/game" component={GameScreen} />
+      <Route path="/room" component={GameScreen} />
     </BrowserRouter>
   </>,
   document.querySelector("#root")
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import "../css/style_index.css";
 
 export default class HomeScreen extends Component {
+  state = {
+    room: ""
+  };
+
+  handleSubmit = async () => {
+    // await axios.post("/room");
+    this.props.history.push(`/room/${this.state.room}`);
+  };
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div id="home_page">
+        <div id="showcase">
+          <h1>Welcome to War of Words!</h1>
+          <p>
+            Play the most intense text battle game online with your friends
+            right now
+          </p>
+          <button id="create_room">CREATE ROOM</button>
+          <p>or</p>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              className="form_field"
+              placeholder="Enter Room ID"
+              name="roomID"
+              autoComplete="off"
+              value={this.state.room}
+              onChange={event => this.setState({ room: event.target.value })}
+            />
+            <input type="submit" id="join_room" value="JOIN ROOM" />
+          </form>
+        </div>
+      </div>
+    );
   }
 }
